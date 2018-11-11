85d3c2561480a4c7bfea8b5445a227ef44e3957c
Taylor Herald <t.herald7@hotmail.com>
2018-11-11 12:57:56 -0600

    Good stopping point

--- js/ideology.js
+++ js/ideology.js
@@ -19,8 +19,8 @@ class Ideology {
             .attr("preserveAspectRatio", "xMinYMin");
         
         // d3 margin convention
-        viz.width = document.getElementById(div).offsetWidth;
-        viz.height = document.getElementById(div).offsetHeight;
+        viz.width = document.getElementById(div.substring(1)).offsetWidth;
+        viz.height = document.getElementById(div.substring(1)).offsetHeight;
 
         viz.data = data;
         viz.draw();
@@ -29,17 +29,65 @@ class Ideology {
     draw() {
         var viz = this;
 
-        var decisionShortDates = viz.data.map(m => m.dateDecision);
-        var decisionYears = decisionShortDates.map(d => d.split("/")[2]);
-        var decisionDistinctYears = new Set(decisionYears);
-        console.log(decisionDistinctYears);
+        d3.csv("./data/ideology_data.csv", function (error, ideology_data) {
+            var years = ideology_data.map(d => d.Year);
+            var parseDate = d3.timeParse("%Y");
+            var xScale = d3.scaleTime()
+                .domain(d3.extent(years, function(d) { return d; }))
+                .range([25, viz.width-25]);
 
-        var xScale = d3.scaleLinear()
-            .domain(d3.min(decisionDistinctYears), d3.max(decisionDistinctYears))
-            .range(0, viz.width);
+            var xTicks = d3.scaleTime()
+                .domain(d3.extent(years, function(d) { return parseDate(d); }))
+                .range([25, viz.width-25]);
 
-        var fillColorClasses = ["fill_red", "fill_gray", "fill_blue"];
+            var xAxis = d3.axisBottom(xTicks).ticks(years.length/2);
+
+            var stack = d3.stack()
+                .keys(["Conservative", "Unspecified", "Liberal"])
+                .offset(d3.stackOffsetWiggle);
+
+            var series = stack(ideology_data);
+
+            console.log(series);
+
+            var mins = series[0].map(d => d[0]);
+            var maxs = series[2].map(d => d[1]);
+
+            var yScale = d3.scaleLinear()
+                .domain([Math.min(... mins), Math.max(... maxs)])
+                .range([viz.height - 75, 0]);
 
 
+            var fillColorClasses = ["fill_blue", "fill_gray", "fill_red"];
+
+            var area = d3.area()
+                .x(function(d) { console.info('in area function: ', d); return xScale(Number(d.data.Year)); })
+                .y0(function(d) { return yScale(d[0]); })
+                .y1(function(d) { return yScale(d[1]); })
+                .curve(d3.curveBasis);
+
+            var index = 0;
+            viz.svg.selectAll("path")
+                .data(series)
+                .enter().append("path")
+                .attr("d", area)
+                .attr("class", function() { return fillColorClasses[index++]; });
+
+            viz.svg.append("g")
+                .attr("class", "axis axis--x")
+                .attr("transform", "translate(0," + (viz.height - 60) + ")")
+                .call(xAxis)
+                .selectAll("text")  
+                .style("text-anchor", "end")
+                .style("font-size", "0.75rem")
+                .attr("dx", "-.8em")
+                .attr("dy", ".15em")
+                .attr("transform", "rotate(-65)");
+
+            viz.svg.append("text")
+                .attr("transform", "translate(" + (viz.width/2) + " ," + viz.height + ")")
+                .style("text-anchor", "middle")
+                .text("Year");
+        });
     }
 }
