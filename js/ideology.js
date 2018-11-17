263506db47a9869ee29c015a9669d6b0ea523c06
Taylor Herald <t.herald7@hotmail.com>
2018-11-16 20:53:43 -0600

    hovering line works

--- js/ideology.js
+++ js/ideology.js
@@ -22,6 +22,9 @@ class Ideology {
         viz.width = document.getElementById(div.substring(1)).offsetWidth;
         viz.height = document.getElementById(div.substring(1)).offsetHeight;
 
+        viz.mousePosX = 0;
+        viz.mousePosY = 0;
+
         viz.data = data;
         viz.draw();
     }
@@ -40,7 +43,7 @@ class Ideology {
                 .domain(d3.extent(years, function(d) { return parseDate(d); }))
                 .range([25, viz.width-25]);
 
-            var xAxis = d3.axisBottom(xTicks).ticks(years.length/2);
+            var xAxis = d3.axisBottom(xTicks).tickValues(xTicks.ticks(years.length/4).concat(xTicks.domain()));
 
             var stack = d3.stack()
                 .keys(["Liberal", "Unspecified", "Conservative"])
@@ -69,7 +72,71 @@ class Ideology {
                 .data(series)
                 .enter().append("path")
                 .attr("d", area)
-                .attr("class", function() { return fillColorClasses[index++]; });
+                .attr("class", function() {
+                    var classList = "ideology_path ";
+                    classList += fillColorClasses[index++]
+                    return classList;
+                })
+                .on("mouseover", function() {
+                    var dataLine = d3.select("#data_line");
+                    //console.log(dataLine);
+
+                    var date = xTicks.invert(d3.mouse(this)[0]);
+                    var year = date.getFullYear().toString();
+
+                    var lineData = [
+                        { 'x': d3.mouse(this)[0], 'y': (series[0][year - series[0][0].data.Year])["0"]},
+                        { 'x': d3.mouse(this)[0], 'y': (series[2][year - series[0][0].data.Year])["1"]}
+                    ]
+
+                    var line = d3.line()
+                        .x(function(d) { return d['x']; })
+                        .y(function(d) { return yScale(d['y']); });
+
+                    if (dataLine._groups[0][0] == null) {
+                        viz.svg.append("path")
+                        .attr("id", "data_line")
+                        .datum(lineData)
+                        .attr('d', line);
+                    }
+                    else {
+                        d3.select("#data_line").datum(lineData);
+                    }
+                })
+                .on("mousemove", function(d,i) {
+                    var dataLine = d3.select("#data_line");
+
+                    viz.mousePosX = d3.mouse(this)[0];
+                    viz.mousePosY = d3.mouse(this)[0];
+
+                    var date = xTicks.invert(d3.mouse(this)[0]);
+                    var year = date.getFullYear().toString();
+
+                    var lineData = [
+                        { 'x': d3.mouse(this)[0], 'y': (series[0][year - series[0][0].data.Year])["0"]},
+                        { 'x': d3.mouse(this)[0], 'y': (series[2][year - series[0][0].data.Year])["1"]}
+                    ]
+
+                    var line = d3.line()
+                        .x(function(d) { return d['x']; })
+                        .y(function(d) { return yScale(d['y']); });
+
+                    if (dataLine._groups[0][0] == null) {
+                        viz.svg.append("path")
+                            .attr("id", "data_line")
+                            .datum(lineData)
+                            .attr('d', line);
+                    }
+                    else {
+                        dataLine.datum(lineData)
+                            .attr('d', line);
+                    }
+                })
+                .on("mouseout", function(d) {
+                    if ((viz.mousePosX < 25 || viz.mousePosX > viz.totalWidth + 25) && (viz.mousePosY < 0 || viz.mousePosY > viz.totalHeight - 75)) {
+                        d3.select("#data_line").remove();
+                    }
+                });
 
             viz.svg.append("g")
                 .attr("class", "axis axis--x")
@@ -83,7 +150,7 @@ class Ideology {
                 .attr("transform", "rotate(-65)");
 
             viz.svg.append("text")
-                .attr("transform", "translate(" + (viz.width/2) + " ," + viz.height + ")")
+                .attr("transform", "translate(" + (viz.width/2) + " ," + (viz.height - 5) + ")")
                 .style("text-anchor", "middle")
                 .text("Year");
         });
